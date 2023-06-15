956eebfd1b6e93ab7ce15abb33060fc686a12eb4
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2023-06-15 17:23:56 -0300

    Requisito03_primeiras_alterações_no_WalletForm

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,115 @@
 import React, { Component } from 'react';
 
 class WalletForm extends Component {
+  state = {
+    description: '',
+    tag: 'Alimentação',
+    value: '',
+    method: 'Dinheiro',
+    currencies: [],
+  };
+
+  componentDidMount() {
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies = async () => {
+    try {
+      const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+      const data = await response.json();
+      const currencies = Object.keys(data).filter((currency) => currency !== 'USDT');
+
+      this.setState({ currencies });
+    } catch (error) {
+      console.error('Não foi possível buscar moedas:', error);
+    }
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { value, description, method, tag, currencies, currency } = this.state;
+
     return (
-      <div>WalletForm</div>
+      // <form onSubmit={ this.handleSubmit }>
+      <form>
+        <label htmlFor="value">
+          Valor
+          <input
+            id="value"
+            type="number"
+            name="value"
+            value={ value }
+            onChange={ (e) => this.handleChange(e) }
+            data-testid="value-input"
+          />
+        </label>
+
+        <label htmlFor="description">
+          Descrição da despesa
+          <input
+            id="description"
+            type="text"
+            name="description"
+            value={ description }
+            onChange={ (e) => this.handleChange(e) }
+            data-testid="description-input"
+          />
+        </label>
+
+        <label htmlFor="method">
+          Método de Pagamento
+          <select
+            id="method"
+            name="method"
+            value={ method }
+            onChange={ (e) => this.handleChange(e) }
+            data-testid="method-input"
+          >
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+
+        <label htmlFor="tag">
+          Categoria da despesa
+          <select
+            id="tag"
+            name="tag"
+            value={ tag }
+            onChange={ (e) => this.handleChange(e) }
+            data-testid="tag-input"
+          >
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+
+        <label htmlFor="currency">
+          Moeda
+          <select
+            id="currency"
+            name="currency"
+            value={ currency }
+            onChange={ this.handleChange }
+            data-testid="currency-input"
+          >
+            {currencies.map((curr, index) => (
+              <option key={ index } value={ curr }>
+                {curr}
+              </option>
+            ))}
+          </select>
+        </label>
+
+      </form>
     );
   }
 }
